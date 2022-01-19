7c23cc8d1f8cec8d1ce5a09c62714800de5909fa
wadhard <wadhard@mail.ru>
2022-01-20 03:14:33 +1000

    prepare for merge and rebase_2.0

--- branching/merge.sh
+++ branching/merge.sh
@@ -1,8 +1,8 @@
-#!/bin/bash
-# display command line options
+#!/bin/bash [ #!/bin/bash] 
+# display command line options [ # display command line options] 
 
-count=1
-for param in "$*"; do
-    echo "\$* Parameter #$count = $param"
-    count=$(( $count + 1 ))
-done
+count=1 [ count\u003d1] 
+for param in "$*"; do [ for param in '$*'; do] 
+    echo "\$* Parameter #$count = $param" [ echo '\\$* Parameter #$count \u003d $param'] 
+    count=$(( $count + 1 )) [ count\u003d$(( $count + 1 ))] 
+done [ done] 
